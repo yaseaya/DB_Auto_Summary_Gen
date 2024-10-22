b5c7f0139faab6b2a0b99e4202dec0d36e97d68d
eBus <yaseaya@gmail.com>
2024-10-22 16:38:55 +0800

    v05 stable version: set 心跳时间600 秒 to avoid MQ connection failed as timeout

--- MQ_Summary_main.py
+++ MQ_Summary_main.py
@@ -17,7 +17,9 @@ def reconnect_MQ():
     retry_count = 0
     while retry_count < 5:
         try:
-            connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))  # 重新连接到RabbitMQ
+            # 在 RabbitMQ 中，心跳时间的缺省值通常为 60 秒。这意味着如果在 60 秒内没有任何活动，RabbitMQ 将关闭连接。
+            # 设置心跳时间为600秒
+            connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', heartbeat=600))  # 重新连接到RabbitMQ
             channel = connection.channel()
             # 重新声明队列和注册消费者
             channel.queue_declare(queue='txt_file_queue')
@@ -48,6 +50,9 @@ def callback_txt_file_queue(ch, method, properties, body):
         ch.basic_ack(delivery_tag=method.delivery_tag)
     except pika.exceptions.StreamLostError:
         print("连接丢失，无法确认消息。")  # 捕获连接丢失异常
+        reconnect_MQ()
+        print("手动确认消息 AGAIN。")
+        ch.basic_ack(delivery_tag=method.delivery_tag)
 
 def callback_av_file_queue(ch, method, properties, body):
     av_file_name = body.decode()  # 解码接收到的消息
@@ -59,25 +64,29 @@ def callback_av_file_queue(ch, method, properties, body):
     except FileNotFoundError:
         print("文件或目录不存在")  # 捕获文件未找到异常
     except Exception as e:
-        print(f"处理消息时发���错误: {e}")  # 捕获其他异常
+        print(f"处理消息时发错误: {e}")  # 捕获其他异常
     
     # 手动确认消息
     try:
         ch.basic_ack(delivery_tag=method.delivery_tag)
     except pika.exceptions.StreamLostError:
         print("连接丢失，无法确认消息。")  # 捕获连接丢失异常
+        reconnect_MQ()
+        print("手动确认消息 AGAIN。")
+        ch.basic_ack(delivery_tag=method.delivery_tag)
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))  # 连接到RabbitMQ
+    # 设置心跳时间为600秒
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', heartbeat=600))  # 连接到RabbitMQ
     channel = connection.channel()
 
     # 清除已存在的txt_file_queue
-    channel.queue_delete(queue='txt_file_queue')
-    # 清除已存在的av_file_queue
-    channel.queue_delete(queue='av_file_queue')
+    # channel.queue_delete(queue='txt_file_queue')
+    # # 清除已存在的av_file_queue
+    # channel.queue_delete(queue='av_file_queue')
 
-    channel.queue_declare(queue='txt_file_queue')  # 声明txt_file_queue
-    channel.queue_declare(queue='av_file_queue')   # 声明av_file_queue
+    # channel.queue_declare(queue='txt_file_queue')  # 声明txt_file_queue
+    # channel.queue_declare(queue='av_file_queue')   # 声明av_file_queue
 
     # 设置预取计数为1，确保一个任务完成后再接收下一个任务
     channel.basic_qos(prefetch_count=1)
@@ -90,7 +99,7 @@ def main():
     try:
         channel.start_consuming()  # 开始消费消息
     except pika.exceptions.ConnectionClosed:
-        print("连接丢失，���试重新连接...")
+        print("连接丢失，试重新连接...")
         # 重新连接的逻辑
         time.sleep(5)
         reconnect_MQ()
@@ -119,7 +128,7 @@ def send_av_message(av_file_name):
     channel.basic_publish(exchange='', routing_key='av_file_queue', body=av_file_name,
         properties=pika.BasicProperties(
             delivery_mode=2,  # 使消息持久化
-        ))  # 发送消���
+        ))  # 发送消
     print(f"Sent {av_file_name} to av_file_queue")
     connection.close()  # 关闭连接
 
